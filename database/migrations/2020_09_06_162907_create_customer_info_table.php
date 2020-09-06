48c543f1ec3ebac273326110109ccf37af5d87c8
haruncse <harunce12008@gmail.com>
2020-09-06 23:22:09 +0600

    Model Added With Table

---
+++ database/migrations/2020_09_06_162907_create_customer_info_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCustomerInfoTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('CustomerInfo', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('customerName', 100);
+            $table->text('customerAddress', 100);
+            $table->enum('customerType', ['Regular', 'Frequent']);
+            $table->dateTime('creationDate', 0);
+            $table->timestamps();
+            $table->charset = 'utf8mb4';    
+            $table->collation = 'utf8mb4_unicode_ci';
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('CustomerInfo');
+    }
+}
