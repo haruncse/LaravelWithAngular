dc40929446d6ec06b3efa4526890fee5adc39f0d
haruncse <harunce12008@gmail.com>
2020-09-06 08:30:59 +0600

    Model Added

---
+++ app/Http/Controllers/CustomerInfoController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Model\CustomerInfo;
+use Illuminate\Http\Request;
+
+class CustomerInfoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Model\CustomerInfo  $customerInfo
+     * @return \Illuminate\Http\Response
+     */
+    public function show(CustomerInfo $customerInfo)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Model\CustomerInfo  $customerInfo
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(CustomerInfo $customerInfo)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Model\CustomerInfo  $customerInfo
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, CustomerInfo $customerInfo)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Model\CustomerInfo  $customerInfo
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(CustomerInfo $customerInfo)
+    {
+        //
+    }
+}
