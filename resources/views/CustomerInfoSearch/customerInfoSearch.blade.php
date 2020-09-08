847726f8294e1aae08ae30047c445c566482c097
haruncse <harunce12008@gmail.com>
2020-09-09 00:01:59 +0600

    Angular Search item With Suggestion Complete

--- resources/views/CustomerInfoSearch/customerInfoSearch.blade.php
+++ resources/views/CustomerInfoSearch/customerInfoSearch.blade.php
@@ -25,42 +25,46 @@
 
 <div class="container">
 	<div class="row">
-		{{--<div class="divShow" ng-app="moduleName" ng-controller="controllerName">
+		{{--
+			<div class="divShow" ng-app="moduleName" ng-controller="controllerName">
 
-			<div >
-				<table>
-					<tr>
-						<th>Customer Name</th> <th> Customer Type</th><th> Customer Address</th>
-					</tr>
-					<tr ng-repeat="item in customerData">
-						<td>@{{item.customerName}}</td><td>@{{item.customerType}}</td><td>@{{item.customerAddress}}</td>
-					</tr>
-				</table>
-			</div>
+				<div >
+					<table>
+						<tr>
+							<th>Customer Name</th> <th> Customer Type</th><th> Customer Address</th>
+						</tr>
+						<tr ng-repeat="item in customerData">
+							<td>@{{item.customerName}}</td><td>@{{item.customerType}}</td><td>@{{item.customerAddress}}</td>
+						</tr>
+					</table>
+				</div>
 			
-			<div class="from-group" >
-				<label>Customer Search </label> 
+				<div class="from-group" >
+					<label>Customer Search </label> 
 
-					<input type="text" name="customerName" id="customerName" ng-model="customerSearch" ng-keyup="customerDataSearch(customerSearch)" class="form-control" />
+						<input type="text" name="customerName" id="customerName" ng-model="customerSearch" ng-keyup="customerDataSearch(customerSearch)" class="form-control" />
 
-					<ul class="list-group">
-						<li class="list-group-item" ng-repeat="countrydata in filterCustomer" ng-click="fillTextbox(countrydata)">@{{countrydata}}</li>
-					</ul>
+						<ul class="list-group">
+							<li class="list-group-item" ng-repeat="countrydata in filterCustomer" ng-click="fillTextbox(countrydata)">@{{countrydata}}</li>
+						</ul>
+				</div>
+
+				<div class="from-group">
+					<button ng-click="searchCustomerInfo()">Search</button>	
+				</div>
 			</div>
+		--}}
 
-			<div class="from-group">
-				<button ng-click="searchCustomerInfo()">Search</button>	
-			</div>
-		</div>--}}
 		<div class="container col-md-6">
-			<div ng-app="customerInfoSearchApp" ng-controller="customerInfoSearchController">
+			<div class="col-md-4" ng-app="customerInfoSearchApp" ng-controller="customerInfoSearchController">
 				<h3 align="center">AngularJS Autocomplete Textbox</h3>
 				<label>Customer Info Search: Enter Customer Info </label>
-				<input type="text" name="country" id="country" ng-model="country" ng-keyup="complete(country)" class="form-control" />
-				<ul class="list-group">
-					<li class="list-group-item" ng-repeat="countrydata in filterCountry" ng-click="fillTextbox(countrydata)">@{{countrydata}}</li>
-				</ul>
-				
+				<div >
+					<input type="text" name="customerInfo" id="customerInfo" ng-model="customerSearchInfo" ng-keyup="searchCustomer(customerSearchInfo)" class="form-control" />
+					<ul class="list-group">
+						<li class="list-group-item" ng-repeat="customerData in filterCoustomer" ng-click="fillTextBox(customerData)">@{{customerData}}</li>
+					</ul>
+				</div>
 			</div>
 		</div>
 	</div>
--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -4,7 +4,7 @@
 	<title>Angular JS Basic</title>
 	<script type="text/javascript" src="/js/angular.min.js"></script>
 	<link rel="stylesheet" href="/css/bootstrap.css" type="text/css">
-	{{--<link rel="stylesheet" href="/css/bootstrap.min.css" type="text/css">--}}
+	<link rel="stylesheet" href="/css/bootstrap.min.css" type="text/css">
 	<link rel="stylesheet" href="/css/bootstrap-responsive.css" type="text/css" media="screen">
 </head>
 	<body>
