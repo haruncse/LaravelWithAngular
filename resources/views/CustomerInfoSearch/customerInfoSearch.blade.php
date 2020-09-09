553e3d58d12d56af04470d38d092a407ca5b30de
haruncse <harunce12008@gmail.com>
2020-09-09 19:41:43 +0600

    Text Field Search Atocomplete

--- resources/views/CustomerInfoSearch/customerInfoSearch.blade.php
+++ resources/views/CustomerInfoSearch/customerInfoSearch.blade.php
@@ -25,36 +25,6 @@
 
 <div class="container">
 	<div class="row">
-		{{--
-			<div class="divShow" ng-app="moduleName" ng-controller="controllerName">
-
-				<div >
-					<table>
-						<tr>
-							<th>Customer Name</th> <th> Customer Type</th><th> Customer Address</th>
-						</tr>
-						<tr ng-repeat="item in customerData">
-							<td>@{{item.customerName}}</td><td>@{{item.customerType}}</td><td>@{{item.customerAddress}}</td>
-						</tr>
-					</table>
-				</div>
-			
-				<div class="from-group" >
-					<label>Customer Search </label> 
-
-						<input type="text" name="customerName" id="customerName" ng-model="customerSearch" ng-keyup="customerDataSearch(customerSearch)" class="form-control" />
-
-						<ul class="list-group">
-							<li class="list-group-item" ng-repeat="countrydata in filterCustomer" ng-click="fillTextbox(countrydata)">@{{countrydata}}</li>
-						</ul>
-				</div>
-
-				<div class="from-group">
-					<button ng-click="searchCustomerInfo()">Search</button>	
-				</div>
-			</div>
-		--}}
-
 		<div class="container col-md-6">
 			<div class="col-md-4" ng-app="customerInfoSearchApp" ng-controller="customerInfoSearchController">
 				<h3 align="center">AngularJS Autocomplete Textbox</h3>
