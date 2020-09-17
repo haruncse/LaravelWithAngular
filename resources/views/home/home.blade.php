56075e0098ef4e98812f0dc7e6f5b50740e5bfca
haruncse <harunce12008@gmail.com>
2020-09-17 22:05:59 +0600

    Bootstrap Container

--- resources/views/home/home.blade.php
+++ resources/views/home/home.blade.php
@@ -2,19 +2,20 @@
 @section('homeContent')
 <script type="text/javascript" src="/js/homeDataFlow.js"></script>
 <div class="container">
-	<div>
-		This is Home Content
-	</div>
-
-	<div ng-app="moduleName" ng-controller="controllerName as dataItem">
-		<li ng-repeat="item in allDataItem">
-			@{{item}}
-		</li>
-		<input type="text" name="" id="myData" ng-model="dataItem.dataText" >
-		<input type="text" name="" id="myData2" ng-model="dataItem.dataText2" >
-		<input type="text" name="" id="myData3" ng-model="dataItem.dataText3" >
-		<button ng-click="addDataItem()" >Add Data</button>
-		<button ng-click="postsave()" >Add Data2</button>
+	<div class="row">
+		<div>
+			This is Home Content
+		</div>
+		<div ng-app="moduleName" ng-controller="controllerName as dataItem">
+			<li ng-repeat="item in allDataItem">
+				@{{item}}
+			</li>
+			<input type="text" name="" id="myData" ng-model="dataItem.dataText" >
+			<input type="text" name="" id="myData2" ng-model="dataItem.dataText2" >
+			<input type="text" name="" id="myData3" ng-model="dataItem.dataText3" >
+			<button ng-click="addDataItem()" >Add Data</button>
+			<button ng-click="postsave()" >Add Data2</button>
+		</div>
 	</div>
 </div>
 @endsection
