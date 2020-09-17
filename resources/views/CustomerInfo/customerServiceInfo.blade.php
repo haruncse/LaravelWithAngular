e4d169c4da0c1f4a2a9fe9fd198a4e264c0d08c3
haruncse <harunce12008@gmail.com>
2020-09-17 20:57:27 +0600

    Bootstrap Container

--- resources/views/CustomerInfo/customerServiceInfo.blade.php
+++ resources/views/CustomerInfo/customerServiceInfo.blade.php
@@ -87,117 +87,117 @@
         medium;margin-top: 10px;
     }
 </style>
+<div class="container">
+	<div class="divShow" ng-app="moduleName" ng-controller="controllerName as customerInfo">
+		Customer Info Service
+		<div class="container mContainer2">
+		    <div class="row">
+		        <div class="displayDiv col-lg-3 col-md-3 col-sm-4 col-xs-4 ">
+		            <label class="col-lg-12 label label-info userName"> My Name </label>
+		            {{--<label class="col-lg-12" onclick="displayInfo('manageAccount');"><a>Manage Account</a></label>--}}
+		            <label class="col-lg-12" onclick="displayInfo('personalInfo');"><a>Personal Information</a></label>
+		            <label class="col-lg-12" onclick="displayInfo('Orders');"><a>Orders</a></label>
+		            <label class="col-lg-12" onclick="displayInfo('Orders');"><a>Re-Order</a></label>
+		            <label class="col-lg-12"><a  href="http://lovelypetbd/logout" onclick="event.preventDefault();document.getElementById('logout-form').submit();">Log Out</a></label>
+		        </div>
+		        <div class="col-lg-9 col-md-9 col-sm-8 col-xs-8 ">
+		            <div id="personalInfo" class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+		                <div class="panel panel-default">
+		                    <div class="panel-heading"><label> Update Your Personal Info</label></div>
 
-<div class="divShow" ng-app="moduleName" ng-controller="controllerName as customerInfo">
-	Customer Info Service
-	<div class="container mContainer2">
-	    <div class="row">
-	        <div class="displayDiv col-lg-3 col-md-3 col-sm-4 col-xs-4 ">
-	            <label class="col-lg-12 label label-info userName"> My Name </label>
-	            {{--<label class="col-lg-12" onclick="displayInfo('manageAccount');"><a>Manage Account</a></label>--}}
-	            <label class="col-lg-12" onclick="displayInfo('personalInfo');"><a>Personal Information</a></label>
-	            <label class="col-lg-12" onclick="displayInfo('Orders');"><a>Orders</a></label>
-	            <label class="col-lg-12" onclick="displayInfo('Orders');"><a>Re-Order</a></label>
-	            <label class="col-lg-12"><a  href="http://lovelypetbd/logout" onclick="event.preventDefault();document.getElementById('logout-form').submit();">Log Out</a></label>
-	        </div>
-	        <div class="col-lg-9 col-md-9 col-sm-8 col-xs-8 ">
-	            <div id="personalInfo" class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-	                <div class="panel panel-default">
-	                    <div class="panel-heading"><label> Update Your Personal Info</label></div>
+		                    <div class="panel-body">
+		                      @if(Session::has('message'))
+		                      <p class="alert {{ Session::get('alert-class', 'alert-info') }}">{{ Session::get('message') }}</p>
+		                      @endif
 
-	                    <div class="panel-body">
-	                      @if(Session::has('message'))
-	                      <p class="alert {{ Session::get('alert-class', 'alert-info') }}">{{ Session::get('message') }}</p>
-	                      @endif
+		                            <div class="form-group{{ $errors->has('mobileNumber') ? ' has-error' : '' }}">
+		                                <label for="userName" class="col-md-4 control-label">Name </label>
 
-	                            <div class="form-group{{ $errors->has('mobileNumber') ? ' has-error' : '' }}">
-	                                <label for="userName" class="col-md-4 control-label">Name </label>
+		                                <div class="col-md-6">
+		                                    <input id="userName" type="text" class="form-control" name="name"  value="" required autofocus>
+		                                </div>
+		                            </div>
 
-	                                <div class="col-md-6">
-	                                    <input id="userName" type="text" class="form-control" name="name"  value="" required autofocus>
-	                                </div>
-	                            </div>
+		                            <div class="form-group{{ $errors->has('mobileNumber') ? ' has-error' : '' }}">
+		                                <label for="mobileNumber" class="col-md-4 control-label">Mobile Number</label>
 
-	                            <div class="form-group{{ $errors->has('mobileNumber') ? ' has-error' : '' }}">
-	                                <label for="mobileNumber" class="col-md-4 control-label">Mobile Number</label>
+		                                <div class="col-md-6">
+		                                    <input id="mobileNumber" type="text" class="form-control" name="mobileNumber" min="11" maxlength="11" value="" required autofocus>
 
-	                                <div class="col-md-6">
-	                                    <input id="mobileNumber" type="text" class="form-control" name="mobileNumber" min="11" maxlength="11" value="" required autofocus>
+		                                </div>
+		                            </div>
 
-	                                </div>
-	                            </div>
+		                            <div class="form-group{{ $errors->has('address') ? ' has-error' : '' }}">
+		                                <label for="address" class="col-md-4 control-label">Address</label>
 
-	                            <div class="form-group{{ $errors->has('address') ? ' has-error' : '' }}">
-	                                <label for="address" class="col-md-4 control-label">Address</label>
+		                                <div class="col-md-6">
+		                                    <input id="address" type="text" class="form-control" name="address" value="" required>
 
-	                                <div class="col-md-6">
-	                                    <input id="address" type="text" class="form-control" name="address" value="" required>
+		                                    @if ($errors->has('address'))
+		                                        <span class="help-block">
+		                                            <strong>{{ $errors->first('address') }}</strong>
+		                                        </span>
+		                                    @endif
+		                                </div>
+		                            </div>
 
-	                                    @if ($errors->has('address'))
-	                                        <span class="help-block">
-	                                            <strong>{{ $errors->first('address') }}</strong>
-	                                        </span>
-	                                    @endif
-	                                </div>
-	                            </div>
+		                             <div class="form-group{{ $errors->has('mobileNumber') ? ' has-error' : '' }}">
+		                                <label for="emailAddress" class="col-md-4 control-label">Email Address </label>
 
-	                             <div class="form-group{{ $errors->has('mobileNumber') ? ' has-error' : '' }}">
-	                                <label for="emailAddress" class="col-md-4 control-label">Email Address </label>
+		                                <div class="col-md-6">
+		                                    <input id="emailAddress" type="text" class="form-control" name="emailAddress"  value="" required autofocus disabled="disabled">
+		                                </div>
+		                            </div>
+		                             <div class="form-group">
+		                                <label for="petName" class="col-md-4 control-label">Pet Name </label>
 
-	                                <div class="col-md-6">
-	                                    <input id="emailAddress" type="text" class="form-control" name="emailAddress"  value="" required autofocus disabled="disabled">
-	                                </div>
-	                            </div>
-	                             <div class="form-group">
-	                                <label for="petName" class="col-md-4 control-label">Pet Name </label>
+		                                <div class="col-md-6">
+		                                    <input id="petName" type="text" class="form-control" name="petName"  value="" >
+		                                </div>
+		                            </div>
+		                             <div class="form-group">
+		                                <label for="petType" class="col-md-4 control-label">Pet Type </label>
 
-	                                <div class="col-md-6">
-	                                    <input id="petName" type="text" class="form-control" name="petName"  value="" >
-	                                </div>
-	                            </div>
-	                             <div class="form-group">
-	                                <label for="petType" class="col-md-4 control-label">Pet Type </label>
+		                                <div class="col-md-6">
+		                                    <input id="petType" type="text" class="form-control" name="petType" placeholder="ex: cat/dog etc"  value="" >
+		                                </div>
+		                            </div>
+		                            <div class="form-group">
+		                                <div class="col-md-6 col-md-offset-4">
+		                                    <button type="button" onclick="updatePersonalInfo()" class="btn btn-primary">
+		                                        Update Personal Info
+		                                    </button>
+		                                </div>
+		                            </div>
+		                    </div>
+		                </div>
+		            </div>
 
-	                                <div class="col-md-6">
-	                                    <input id="petType" type="text" class="form-control" name="petType" placeholder="ex: cat/dog etc"  value="" >
-	                                </div>
-	                            </div>
-	                            <div class="form-group">
-	                                <div class="col-md-6 col-md-offset-4">
-	                                    <button type="button" onclick="updatePersonalInfo()" class="btn btn-primary">
-	                                        Update Personal Info
-	                                    </button>
-	                                </div>
-	                            </div>
-	                    </div>
-	                </div>
-	            </div>
+		            <div id="Orders" class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+		                <div class="panel panel-default">
+		                    <div class="panel-heading"><label> All Order List</label></div>
 
-	            <div id="Orders" class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-	                <div class="panel panel-default">
-	                    <div class="panel-heading"><label> All Order List</label></div>
+		                    <div class="panel-body">
+		                      @if(Session::has('message'))
+		                      <p class="alert {{ Session::get('alert-class', 'alert-info') }}">{{ Session::get('message') }}</p>
+		                      @endif
+		                        <form class="form-horizontal" method="POST" action="/">
+		                            {{ csrf_field() }}
 
-	                    <div class="panel-body">
-	                      @if(Session::has('message'))
-	                      <p class="alert {{ Session::get('alert-class', 'alert-info') }}">{{ Session::get('message') }}</p>
-	                      @endif
-	                        <form class="form-horizontal" method="POST" action="/">
-	                            {{ csrf_field() }}
+		                            <div class="form-group">
+		                                
+		                                <div class="col-md-10">
+		                                    <label>All order List</label>
+		                                </div>
 
-	                            <div class="form-group">
-	                                
-	                                <div class="col-md-10">
-	                                    <label>All order List</label>
-	                                </div>
-
-	                            </div>
-	                        </form>
-	                    </div>
-	                </div>
-	            </div>
-	        </div>
-	    </div>
+		                            </div>
+		                        </form>
+		                    </div>
+		                </div>
+		            </div>
+		        </div>
+		    </div>
+		</div>
 	</div>
 </div>
-
 @endsection
--- resources/views/home/home.blade.php
+++ resources/views/home/home.blade.php
@@ -1,19 +1,20 @@
 @extends('layout.app')
 @section('homeContent')
 <script type="text/javascript" src="/js/homeDataFlow.js"></script>
-<div>
-	This is Home Content
-</div>
+<div class="container">
+	<div>
+		This is Home Content
+	</div>
 
-<div ng-app="moduleName" ng-controller="controllerName as dataItem">
-	<li ng-repeat="item in allDataItem">
-		@{{item}}
-	</li>
-	<input type="text" name="" id="myData" ng-model="dataItem.dataText" >
-	<input type="text" name="" id="myData2" ng-model="dataItem.dataText2" >
-	<input type="text" name="" id="myData3" ng-model="dataItem.dataText3" >
-	<button ng-click="addDataItem()" >Add Data</button>
-	<button ng-click="postsave()" >Add Data2</button>
+	<div ng-app="moduleName" ng-controller="controllerName as dataItem">
+		<li ng-repeat="item in allDataItem">
+			@{{item}}
+		</li>
+		<input type="text" name="" id="myData" ng-model="dataItem.dataText" >
+		<input type="text" name="" id="myData2" ng-model="dataItem.dataText2" >
+		<input type="text" name="" id="myData3" ng-model="dataItem.dataText3" >
+		<button ng-click="addDataItem()" >Add Data</button>
+		<button ng-click="postsave()" >Add Data2</button>
+	</div>
 </div>
-
 @endsection
