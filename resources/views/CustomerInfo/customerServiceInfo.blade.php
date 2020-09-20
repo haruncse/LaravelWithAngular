941fc86c2750f19321acdd6527e94a529a44ff97
haruncse <harunce12008@gmail.com>
2020-09-20 20:18:47 +0600

    Customer Responsive Table wit Data

--- resources/views/CustomerInfo/customerServiceInfo.blade.php
+++ resources/views/CustomerInfo/customerServiceInfo.blade.php
@@ -181,9 +181,30 @@
 			                    <div class="panel-heading"><label > All Order List</label></div>
 
 			                    <div class="panel-body">
-			                    <table class="table-responsive">
-			                    	All Oreder Will Goes here by angular
-			                    </table>
+			                    	<div class="table-responsive">
+				                    	<table class="table">
+										    <thead>
+										      <tr>
+										        <th>#</th>
+										        <th>Firstname</th>
+										        <th>Lastname</th>
+										        <th>Age</th>
+										        <th>City</th>
+										        <th>Country</th>
+										      </tr>
+										    </thead>	
+										     <tbody>
+										      <tr>
+										        <td>1</td>
+										        <td>Anna</td>
+										        <td>Pitt</td>
+										        <td>35</td>
+										        <td>New York</td>
+										        <td>USA</td>
+										      </tr>
+										    </tbody>                    
+										</table>
+									</div>
 			                    </div>
 			                </div>
 			            </div>
